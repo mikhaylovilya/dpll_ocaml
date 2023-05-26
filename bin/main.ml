aeeeeec58d9a4f69dcbfa2e41a4fa4d4116710e9
cyyyyyyyyyyyyy <ily2407@gmail.com>
2023-05-26 15:39:34 +0100

    optimizatons performed #1

--- bin/main.ml
+++ bin/main.ml
@@ -1,22 +1,21 @@
 open Base
 open Stdio
 
-let debug_mode = 1
+let debug_mode = 0
 
 (* type mystruct = int list list *)
 type cnf_options = { vars_num : int; cls_num : int }
 type formula = { cnf_options : cnf_options; clauses : int list list }
 type cnf_result = Unsat | Sat of int list
 
+let print_lst lst =
+  Caml.Format.printf "%s\n"
+  @@ List.fold_right lst
+       ~f:(fun x acc -> String.concat ~sep:" " [ x; acc ])
+       ~init:""
+
 let log str = if debug_mode = 1 then Caml.Format.printf "%s\n" str
-
-let log_lst lst =
-  if debug_mode = 1 then
-    Caml.Format.printf "%s\n"
-    @@ List.fold_right lst
-         ~f:(fun x acc -> String.concat ~sep:" " [ x; acc ])
-         ~init:""
-
+let log_lst lst = if debug_mode = 1 then print_lst lst
 let insert ~cls ~acc = cls :: acc
 
 let parse_cnf ~path =
@@ -27,8 +26,8 @@ let parse_cnf ~path =
     | Some x -> (
         match String.split_on_chars ~on:[ ' ' ] x with
         | [ "p"; "cnf"; vars_num; cls_num ] ->
-            let _ = log "header" in
-            let _ = log_lst [ vars_num; cls_num ] in
+            (* let _ = log "header" in *)
+            (* let _ = log_lst [ vars_num; cls_num ] in *)
             Some
               {
                 vars_num = Int.of_string vars_num;
@@ -43,8 +42,8 @@ let parse_cnf ~path =
         match List.tl @@ List.rev @@ String.split_on_chars ~on:[ ' ' ] x with
         | None -> None
         | Some line ->
-            let _ = log "clause" in
-            let _ = log_lst line in
+            (* let _ = log "clause" in *)
+            (* let _ = log_lst line in *)
             parse_clauses
             @@ insert ~cls:(List.map line ~f:(fun s -> Int.of_string s)) ~acc)
   in
@@ -68,33 +67,55 @@ module CNFFormula = struct
   let does_contain_empty_clause f =
     let rec helper = function
       | [] -> false
-      | hd :: tl -> if List.length hd = 0 then true else helper tl
+      | hd :: tl -> (
+          (* if List.length hd = 0 then true else helper tl *)
+          match hd with [] -> true | _ -> helper tl)
     in
     helper f.clauses
 
   let does_contain_unit_clause f =
     let rec helper = function
       | [] -> None
-      | hd :: tl -> if List.length hd = 1 then List.hd hd else helper tl
+      | hd :: tl -> (
+          (* if List.length hd = 1 then List.hd hd else helper tl *)
+          match hd with [ x ] -> Some x | _ -> helper tl)
     in
     helper f.clauses
 
-  let substitute l f =
+  let substitute_pure l f =
     let no_pure_occurence =
       List.filter f.clauses ~f:(fun cls ->
+          match List.find cls ~f:(fun lit -> lit = l) with
+          | None -> true
+          | Some cls -> false)
+      (* List.length cls
+         = List.length (List.filter cls ~f:(fun lit -> not (lit = l)))) *)
+      (*todo: rewrite, length is O(n)*)
+    in
+    (* let _ = log (String.concat ~sep:" " [ "Substitute"; Int.to_string l ]) in *)
+    (* let _ =
+         List.iter no_occurence ~f:(fun cls ->
+             log_lst @@ List.map cls ~f:Int.to_string)
+       in *)
+    { cnf_options = f.cnf_options; clauses = no_pure_occurence }
+
+  let substitute l f =
+    let no_pure_occurence =
+      substitute_pure l f
+      (* List.filter f.clauses ~f:(fun cls ->
           List.length cls
-          = List.length (List.filter cls ~f:(fun lit -> not (lit = l))))
+          = List.length (List.filter cls ~f:(fun lit -> not (lit = l)))) *)
       (*todo: rewrite, length is O(n)*)
     in
     let no_occurence =
-      List.map no_pure_occurence ~f:(fun cls ->
+      List.map no_pure_occurence.clauses ~f:(fun cls ->
           List.filter cls ~f:(fun lit -> not (lit = -l)))
     in
-    let _ = log "Substitute" in
-    let _ =
-      List.iter no_occurence ~f:(fun cls ->
-          log_lst @@ List.map cls ~f:Int.to_string)
-    in
+    (* let _ = log (String.concat ~sep:" " [ "Substitute"; Int.to_string l ]) in *)
+    (* let _ =
+         List.iter no_occurence ~f:(fun cls ->
+             log_lst @@ List.map cls ~f:Int.to_string)
+       in *)
     { cnf_options = f.cnf_options; clauses = no_occurence }
 
   let unit_propagation l f =
@@ -120,16 +141,17 @@ module CNFFormula = struct
               in
               Hash_set.add all_lits lit))
     in
-    let _ = log "pure literals" in
-    let _ = log_lst @@ List.map (Hash_set.to_list pure_lits) ~f:Int.to_string in
+    (* let _ = log "pure literals" in *)
+    (* let _ = log_lst @@ List.map (Hash_set.to_list pure_lits) ~f:Int.to_string in *)
     pure_lits
 
-  let pure_literal_elimination l f = substitute l f (*ok?*)
+  let pure_literal_elimination l f = substitute_pure l f (*ok?*)
   let choose f = List.hd_exn @@ List.hd_exn f.clauses
 end
 
 let solve f =
   let pure_literals = CNFFormula.get_pure_literals f in
+  (* let _ =  *)
   let rec loop f pure_literals acc : cnf_result =
     if CNFFormula.is_satisfiable f then Sat acc
     else if CNFFormula.does_contain_empty_clause f then Unsat
@@ -153,13 +175,13 @@ let solve f =
   loop f pure_literals []
 
 let print_model = function
-  | None -> log "Error during parsing file"
+  | None -> print_endline "Error during parsing file"
   | Some cnf_res -> (
       match cnf_res with
-      | Unsat -> log "Unsat"
+      | Unsat -> print_endline "Unsat"
       | Sat res ->
-          let _ = log "Sat" in
-          log_lst @@ List.map res ~f:Int.to_string)
+          let _ = print_endline "Sat" in
+          print_lst @@ List.map res ~f:Int.to_string)
 
 let main ~path =
   match parse_cnf ~path with
