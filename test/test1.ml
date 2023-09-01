7b3061f8d469e5ce0975310907bc931f290bedd9
mikhaylovilya <ily2407@gmail.com>
2023-09-01 14:54:31 +0800

    optimizatons performed #1

--- test/test1.ml
+++ test/test1.ml
@@ -1,5 +1,23 @@
 open Main
 
-let () = print_model @@ main ~path:"../TestFiles/example.txt"
-let () = print_model @@ main ~path:"../TestFiles/examplePureLiteral.txt"
-let () = print_model @@ main ~path:"../TestFiles/exampleUnitLiteral.txt"
+let path = "../TestFiles/"
+
+(* let list_txt =
+     Sys.readdir path |> Array.to_list
+     |> List.filter_map (fun x ->
+            match Filename.extension x = ".txt" with
+            | true -> Some (path ^ x)
+            | false -> None)
+
+   (* let _ = List.iter list_txt ~f: *)
+   let _ = List.iter (fun x -> print_model @@ main ~path:x) list_txt *)
+
+(* let _ = print_model @@ main ~path:(path ^ "sat_100_449.txt") *)
+let _ = print_model @@ main ~path:(path ^ "sat_20_91.txt")
+
+(* let _ = print_model @@ main ~path:(path ^ "unsat_1_2.txt") *)
+(* let _ = print_model @@ main ~path:(path ^ "unsat_2_3.txt") *)
+let _ = print_model @@ main ~path:(path ^ "unsat_50_100.txt")
+
+(* let _ = print_model @@ main ~path:(path ^ "unsat_100_160.txt") *)
+(* let _ = print_model @@ main ~path:(path ^ "sat_100_499.txt") *)
