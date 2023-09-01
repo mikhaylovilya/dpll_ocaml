829ca236b0d90987c9bfa4730eb868730bb825b5
mikhaylovilya <ily2407@gmail.com>
2023-09-01 14:54:31 +0800

    implemented dpll algorithm

--- test/test1.ml
+++ test/test1.ml
@@ -1,6 +1,5 @@
-(* let _ = does_contain_pure_literal main () *)
 open Main
 
-let _ = main ~path:"../TestFiles/example.txt"
-let _ = main ~path:"../TestFiles/examplePureLiteral.txt"
-let _ = main ~path:"../TestFiles/exampleUnitLiteral.txt"
+let () = print_model @@ main ~path:"../TestFiles/example.txt"
+let () = print_model @@ main ~path:"../TestFiles/examplePureLiteral.txt"
+let () = print_model @@ main ~path:"../TestFiles/exampleUnitLiteral.txt"
