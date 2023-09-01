97f0eb0e562ec539031386c57a1fd68df7de5be8
mikhaylovilya <ily2407@gmail.com>
2023-09-01 14:54:31 +0800

    invert clause_intersects and add more verbose debug

--- test/test1.ml
+++ test/test1.ml
@@ -13,10 +13,10 @@ let path = "../TestFiles/"
    let _ = List.iter (fun x -> print_model @@ main ~path:x) list_txt *)
 
 (* let _ = print_model @@ main ~path:(path ^ "sat_100_449.txt") *)
-(* let _ = print_model @@ main ~path:(path ^ "sat_20_91.txt") *)
+let _ = print_model @@ main ~path:(path ^ "sat_20_91.txt")
 
 (* let _ = print_model @@ main ~path:(path ^ "unsat_1_2.txt") *)
 (* let _ = print_model @@ main ~path:(path ^ "unsat_2_3.txt") *)
-let _ = print_model @@ main ~path:(path ^ "unsat_50_100.txt")
+(* let _ = print_model @@ main ~path:(path ^ "unsat_50_100.txt") *)
 (* let _ = print_model @@ main ~path:(path ^ "unsat_100_160.txt") *)
 (* let _ = print_model @@ main ~path:(path ^ "sat_100_499.txt") *)
